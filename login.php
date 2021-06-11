bb2ef4343689ec02f1f9db117b2f39701151b65f
Gabriela-Serrano-Lopez <75129974+Gabriela-Serrano-Lopez@users.noreply.github.com>
2021-06-11 12:39:41 -0500

    main

--- login.php
+++ login.php
@@ -32,7 +32,7 @@ if(isset($_SESSION["cajero_login"]))	//Condicion personal
 }
 if(isset($_SESSION["usuarios_login"]))	//Condicion Usuarios
 {
-	header("location: usuarios/usuarios_portada.php");
+	header("location: usuarios/usu_portada.php");
 }
 
 if(isset($_REQUEST['btn_login']))	
@@ -91,12 +91,12 @@ if(isset($_REQUEST['btn_login']))
 							case "3":
 								$_SESSION["cajero_login"]=$email;				
 								$loginMsg="Usuario: Inicio sesión con éxito";	
-								header("refresh:3;usuarios/usuarios_portada.php");		
+								header("refresh:3;usuarios/usuportada.php");		
 								break;
 							case "4":
 									$_SESSION["usuarios_login"]=$email;				
 									$loginMsg="Usuario: Inicio sesión con éxito";	
-									header("refresh:3;usuarios/usuarios_portada.php");		
+									header("refresh:3;usuarios/usuportada.php");		
 									break;
 								
 							default:

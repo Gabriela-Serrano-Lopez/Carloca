47bebf3fe4db14a5289e78d32a84d07acab9ffc8
Gabriela-Serrano-Lopez <75129974+Gabriela-Serrano-Lopez@users.noreply.github.com>
2021-06-11 12:40:39 -0500

    main

--- usuarios/usu_portada.php
+++ usuarios/usu_portada.php
@@ -4,7 +4,7 @@
 <div class="content-container" style=" text-aling: center;width:100%;padding-left:5px">
             <div class="container">
                 <div class="panel panel-default">
-                   <?php include ("producto.php")?>
+              
 </div>
     
 </div>
